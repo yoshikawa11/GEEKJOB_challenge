a877dc6f0f473d0988ac6819815cc261b6aa8146
yoshikawa masashi <realtanua2632@gmail.com>
2018-02-06 11:48:35 +0900

    課題クラス小問2を提出します

---
+++ CampChallenge_java/013.クラス/Challenge13_2.java
@@ -0,0 +1,26 @@
+
+public class Challenge13_2 {
+    
+    
+    public static void main(String[] args){
+        
+        Man AA = new Man();
+        AA.clear();
+        
+     }
+
+}
+
+
+class Man extends Hito{
+    
+    public void clear(){
+        
+        setHito(null,0);
+            
+    }
+    
+}
+
+
+
