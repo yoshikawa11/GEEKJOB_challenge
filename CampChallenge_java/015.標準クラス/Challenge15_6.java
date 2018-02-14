3f4af5a30511aa84ae16133ced054722d3dcefd5
yoshikawa masashi <realtanua2632@gmail.com>
2018-02-14 12:11:08 +0900

    標準クラス小問6を提出します

---
+++ CampChallenge_java/015.標準クラス/Challenge15_6.java
@@ -0,0 +1,14 @@
+
+package import_date;
+
+public class Challenge15_6 {
+    
+    public static void main(String[] args){
+        
+        String mailAddress = "mailaddress@yahoo.co.jp";
+        String str = mailAddress.substring(11);
+        System.out.print(str.length());
+        
+}
+    
+}
