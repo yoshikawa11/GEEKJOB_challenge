1dc373cdfa35e7de4244e083f672bef454362782
yoshikawa masashi <realtanua2632@gmail.com>
2018-02-14 11:02:00 +0900

    標準クラス小問1を修正したものを提出します

---
+++ CampChallenge_java/015.標準クラス/Challenge15_1modified.java
@@ -0,0 +1,27 @@
+
+package import_date;
+import java.util.Calendar;
+import java.util.Date;
+
+
+
+public class Challenge15_1modified {
+    
+    public static void main(String[] args){
+    
+    Calendar c = Calendar.getInstance();
+    Date d = new Date();
+    
+    c.set(2016,0,01,0,0,0);
+
+    d = c.getTime();
+    System.out.print(d.getTime());
+    
+            
+    }
+   
+    }
+    
+    
+    
+
