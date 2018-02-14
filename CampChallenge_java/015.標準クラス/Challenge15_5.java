fe5379da86f097606df71a56766a8d8568e34b4e
yoshikawa masashi <realtanua2632@gmail.com>
2018-02-14 12:01:06 +0900

    標準クラス小問5を提出します

---
+++ CampChallenge_java/015.標準クラス/Challenge15_5.java
@@ -0,0 +1,18 @@
+
+package import_date;
+
+
+public class Challenge15_5 {
+    
+    public static void main(String[] args){
+    
+    String name = "吉川将司";
+    
+    System.out.println(name.getBytes().length);
+    System.out.print(name.length());
+    
+    }
+
+    
+}
+
