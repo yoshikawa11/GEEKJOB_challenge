6680752c70f390639fd8278d58b763491e5d57ce
yoshikawa masashi <realtanua2632@gmail.com>
2018-02-26 13:24:19 +0900

    DB操作課題1を修正、提出します

---
+++ CampChallenge_java/018.DB操作/Challenge18_1modified1.java
@@ -0,0 +1,26 @@
+package DB_assignment;
+import java.sql.*;
+
+
+
+public class Challenge18_1modified1 {
+    
+    public static void main (String[] args){
+        
+        Connection db_con = null;
+        PreparedStatement db_set = null;
+        ResultSet db_data = null;
+        
+    try
+    {
+    
+        Class.forName("com.sql.jdc.Driver").newInstance();
+        db_con = DriverManager.getConnection("jdc:mysql://localhost:8889/Challenge_db", "masashi", "1192");
+    
+    }catch(SQLException e_sql){
+        System.out.println("接続時にエラーが発生しました:"+e_sql.toString());
+    }catch(Exception e){
+    
+    }
+}
+}
