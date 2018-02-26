4ce934bf7c54438d7d17e7db698ad4783d686e94
yoshikawa masashi <realtanua2632@gmail.com>
2018-02-26 13:56:51 +0900

    DB操作課題全検索の取得と表示を提出します

---
+++ CampChallenge_java/018.DB操作/Challenge18_3.java
@@ -0,0 +1,52 @@
+
+package DB_assignment;
+import java.sql.*;
+
+
+public class Challenge18_3 {
+    
+    public static void main (String[] args){
+        
+        Connection db_con = null;
+        PreparedStatement db_st = null;
+        PreparedStatement db_ss = null;
+        ResultSet db_data = null;
+        
+    try
+    {
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        db_con = DriverManager.getConnection("jdbc:mysql://localhost:8889/Challenge_db", "masashi", "1192");
+        
+        db_st = db_con.prepareStatement("select * from profiles");
+        
+        db_data = db_st.executeQuery();
+        
+        while(db_data.next()){
+            System.out.println(db_data.getString("profilesID"));
+            System.out.println(db_data.getString("name"));
+            System.out.println(db_data.getString("tel"));
+            System.out.println(db_data.getString("age"));
+            System.out.println(db_data.getString("birthday"));
+            
+        }
+        
+    
+    }catch(SQLException e_sql){
+        System.out.println("接続時にエラーが発生しました:"+e_sql.toString());
+    }catch(Exception e){
+        System.out.println(e);
+    }finally{
+     if(db_con != null){
+       try{
+       db_con.close();
+       db_st.close();
+       System.out.println("close");
+       }catch(Exception e){
+        System.out.println("close error");  
+       }
+     }
+}
+}
+}
+    
+
